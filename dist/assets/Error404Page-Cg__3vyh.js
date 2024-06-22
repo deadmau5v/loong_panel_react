76eee0196cec34314d87ed0cf07fe5257e23a4ff
deadmau5v <3230772301@qq.com>
2024-06-22 21:04:36 +0800

    feat: 重构terminal界面
    重构terminal界面

--- dist/assets/Error404Page-Cg__3vyh.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-DCRMDEnN.js";import"./react-dom-B_KBoKIY.js";function a(){return console.log("Error404"),e.jsxs("h1",{children:["错误 ",">_<"," 404"]})}export{a as default};
--- src/pages/TerminalPage.tsx
+++ src/pages/TerminalPage.tsx
@@ -1,122 +1,79 @@
-import {Terminal} from "@xterm/xterm"
-import {useContext, useEffect, useState} from "react";
-import {ConfigContext} from "../config.tsx";
-import {ProCard} from "@ant-design/pro-components";
-import {Button, Divider} from "antd";
-import {useAuth} from "../plugins/AuthContext.tsx";
+import {useState, useContext, useEffect} from 'react';
+import {Button, Modal} from 'antd';
+import {ProForm, ProFormText} from '@ant-design/pro-form';
+import {ProCard} from '@ant-design/pro-components';
+import {ConfigContext} from '../config.tsx';
+import {useAuth} from '../plugins/AuthContext.tsx';
+import {Terminal} from '@xterm/xterm';
 import {FitAddon} from 'xterm-addon-fit';
 
-type screen = {
-    name: string,
-    id: number
-}
-
 export default function Page() {
-    const config = useContext(ConfigContext);
-    const [screen, setScreen] = useState([])
-    const [term,] = useState(() => new Terminal());
-
-    // 验证是否登录
+    const [isModalVisible, setIsModalVisible] = useState(false);
+    const [term] = useState(() => new Terminal({
+        fontSize: 20,
+        fontFamily: 'monospace',
+    }));
     const [webSocket, setWebSocket] = useState<WebSocket | undefined>();
-    const {logined} = useAuth()
+    const {logined} = useAuth();
+    const config = useContext(ConfigContext);
+    const fitAddon = new FitAddon();
+
     if (!logined) {
-        window.location.href = "/login"
+        window.location.href = '/login';
     }
-    const fitAddon = new FitAddon()
 
     useEffect(() => {
         const terminalDiv = document.getElementById('terminal');
-
-        window.addEventListener('resize', () => {
-            fitAddon.fit();
-        });
-
+        window.addEventListener('resize', () => fitAddon.fit());
         if (terminalDiv) {
-            term.loadAddon(fitAddon)
+            term.loadAddon(fitAddon);
             term.open(terminalDiv);
-            fitAddon.fit()
-
-            term.clear();
-            term.writeln("欢迎使用 LoongPanel 终端 ...")
+            fitAddon.fit();
+            term.writeln('欢迎使用 LoongPanel 终端 ...');
         }
-        getScreen()
-    }, [])
-    let getWsUrl: (id: number) => string;
-    const getApiUrl = (endpoint: string) => `${config?.API_URL}/api/v1/${endpoint}`;
-    if (config?.WS_URL != "") {
-        getWsUrl = (id: number) => `${config?.WS_URL}/api/ws/screen?id=${id}`;
-    } else {
-        getWsUrl = (id: number) => `ws://${window.location.host}/api/ws/screen?id=${id}`;
-    }
+    }, []);
 
-    const getScreen = async () => {
-        const response = await fetch(getApiUrl("screen/screen"), {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': localStorage.getItem("SESSION") || ""
-            }
-        });
-        if (response.ok) {
-            const data = await response.json();
-            setScreen(data);
-        }
+    const showModal = () => {
+        setIsModalVisible(true);
     };
 
-    const createScreen = async () => {
-        const response = await fetch(getApiUrl("screen/screen"), {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': localStorage.getItem("SESSION") || ""
-            }
-        });
-        if (response.ok) {
-            const data = await response.json();
-            console.log(data);
-            await getScreen();
-        }
-    }
-
-    const initializeWebSocket = (id: number) => {
-        const ws = new WebSocket(getWsUrl(id));
+    const handleOk = (values: { host: string, port: string, user: string, pwd: string }) => {
+        setIsModalVisible(false);
+        const {host, port, user, pwd} = values;
+        webSocket?.close();
+        term.clear();
+        term.writeln(`🔄   正在连接到窗口 [${user}@${host}:${port}] ...`);
+        const wsUrl = config?.WS_URL ? `${config.WS_URL}/api/ws/screen?host=${host}&port=${port}&user=${user}&pwd=${pwd}` :
+            `ws://${window.location.host}/api/ws/screen?host=${host}&port=${port}&user=${user}&pwd=${pwd}`;
+        const ws = new WebSocket(wsUrl);
         ws.onmessage = event => term.write(event.data);
         term.onData(data => ws.send(data));
         setWebSocket(ws);
-    }
+        term.writeln('✅   链接成功.');
+        fitAddon.fit();
+    };
 
-    const changeScreen = (id: number) => {
-        webSocket?.close();
-        term.clear();
-        term.writeln("🔄   正在连接到窗口 ...");
-        term.clear();
-        term.writeln(`\n🔄   正在连接到窗口 [${id}] ...`);
-        initializeWebSocket(id);
-        term.writeln("✅   链接成功.");
-        fitAddon.fit()
-        term.clear();
-    }
+    const handleCancel = () => {
+        setIsModalVisible(false);
+    };
 
     return (
         <>
-            <ProCard gutter={20}>
-                <ProCard id="TerminalCard" direction="column">
-                    <link rel="stylesheet" href="https://cdn1.d5v.cc/CDN/Project/LoongPanel/static/xterm.css"/>
-                    <div id="terminal" style={{
-                        height: "100%",
-                        width: "100%",
-                    }}/>
-                </ProCard>
-                <ProCard direction="column" id="buttons" style={{maxWidth: "20%"}}>
-                    <Button type="primary" onClick={createScreen} id="createScreen">创建窗口</Button>
-                    <Divider/>
-                    {
-                        screen.map((item: screen) => (
-                            <Button key={item.id} onClick={() => changeScreen(item.id)}>{item.name}</Button>
-                        ))
-                    }
-                </ProCard>
+            <Button type="primary" onClick={showModal} style={{margin: 10}}>
+                连接 SSH
+            </Button>
+            <Modal title="输入 SSH 服务器信息" visible={isModalVisible} onCancel={handleCancel} footer={null}>
+                <ProForm onFinish={handleOk}>
+                    <ProFormText name="host" label="Host" initialValue="127.0.0.1"/>
+                    <ProFormText name="port" label="Port" initialValue="22"/>
+                    <ProFormText name="user" label="User" initialValue="root"/>
+                    <ProFormText name="pwd" label="Password" fieldProps={{type: 'password'}}/>
+                </ProForm>
+            </Modal>
+            <ProCard id="TerminalCard">
+                <link rel="stylesheet" href="https://cdn1.d5v.cc/CDN/Project/LoongPanel/static/xterm.css"/>
+                <div id="terminal" style={{height: '700px', width: '100%'}}/>
             </ProCard>
         </>
-    )
+    );
 }
